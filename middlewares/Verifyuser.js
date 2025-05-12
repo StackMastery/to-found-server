c1e6cf4f40d2d1220f5ad3563cc720c7cb8ef7bb
= <=>
2025-05-12 11:05:53 +0600

    Server issue solved

--- middlewares/Verifyuser.js
+++ middlewares/Verifyuser.js
@@ -1,27 +1,25 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
 // User Jwt Token Verify Middleware
 const VerifyUser = async (req, res, next) => {
-    const token = req?.cookies?.token
-    const { uid } = req.query
+  const token = req?.cookies?.token;
+  const { uid } = req.query;
 
-    if(!token || !uid){
-        return res.status(200).send('Unautorized user')
+  // if(!token || !uid){
+  //     return res.status(200).send('Unautorized user')
+  // }
+
+  try {
+    const decodeToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (decodeToken.uid === uid) {
+      return next();
+    } else {
+      return res.status(200).send("Unautorized user");
     }
+  } catch (err) {
+    return res.status(200).send("Unautorized user");
+  }
+};
 
-    try{
-        const decodeToken = jwt.verify(token, process.env.JWT_SECRET)
-
-        if(decodeToken.uid === uid){
-            return next()
-        }else{
-            return res.status(200).send('Unautorized user')
-        }
-    }
-    catch(err){
-        return res.status(200).send('Unautorized user')
-    }
-
-}
-
-export { VerifyUser }
+export { VerifyUser };
